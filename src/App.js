425ef2f8cff69b2f28b79b14b94e8abb4bf7a036
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-09-21 16:58:31 -0500

    Add basic styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return <div>:D</div>;
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
 render(<App />, document.getElementById("root"));
