df64618d44eccabd36c22209b6efd69c19532663
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-09-21 18:58:36 -0500

    Start with hooks: eslint configuration and useState

---
+++ src/SearchParams.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
